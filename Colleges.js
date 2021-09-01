21f272a52cf8a926587959202a78817bcc176bfe
MohitSharmaRishi <85992036+MohitSharmaRishi@users.noreply.github.com>
2021-09-02 01:52:09 +0530

    Add files via upload

---
+++ Colleges.js
@@ -0,0 +1,18 @@
+﻿[
+  {
+    "College_Name": "SRMU",
+    "Annual_Fee": "10000",
+    "Caste": "GEN",
+    "Course": "B.Tech",
+    "Scholarship_Amount": "50000",
+    "Scholarship_Marks": "88.00"
+  },
+  {
+    "College_Name": "BBD",
+    "Annual_Fee": "10000",
+    "Caste": "GEN",
+    "Course": "BSC",
+    "Scholarship_Amount": "50000",
+    "Scholarship_Marks": "88.00"
+  }
+]
